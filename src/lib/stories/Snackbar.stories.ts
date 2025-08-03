d5a55885308ba1ca6f6620e633ddc059feb2fb31
iyodatakashi <iyoda.takashi@gmail.com>
2025-08-04 02:08:01 +0900

    snackbar.tsをsnackbar.svelte.tsに変更（修正漏れ）

--- src/lib/stories/Snackbar.stories.ts
+++ src/lib/stories/Snackbar.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/sveltekit';
 import Snackbar from '../components/Snackbar.svelte';
-import { snackbarManager } from '../utils/snackbar';
+import { snackbarManager } from '../utils/snackbar.svelte';
 
 // グローバルWindow型を拡張
 declare global {
